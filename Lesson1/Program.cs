097d48db367bfd00f4a9eec8686f9c72c6fbd75d
UWB <UWB@UWB-EMILY>
2016-11-11 10:32:56 -0500

    Add project files.

---
+++ Lesson1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("To start the stop Watch, enter start: ");
+            var stopWatch = new StopWatch();
+            var userInput = Console.ReadLine();
+            while (userInput == "start")
+            {
+                stopWatch.start();
+                userInput = Console.ReadLine();
+            }
+                //this end method
+                stopWatch.stop();
+            
+            
+        }
+    }
+}
---
+++ Lesson1/StopWatch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    public class StopWatch
+    {
+        //public string UserInput;
+        DateTime time = new DateTime();
+        private DateTime startTime;
+        private DateTime endTime;
+        private TimeSpan elapsTime;
+
+        public void start()
+        {
+            startTime = DateTime.Now;
+            Console.WriteLine("Stop Watch is running...");
+
+        }
+
+        public void stop()
+        {
+            endTime = DateTime.Now;
+            elapsTime = endTime - startTime;
+            Console.WriteLine(elapsTime);
+        }
+    }
+}
